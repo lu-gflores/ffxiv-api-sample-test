91590731be837a833f734017e47f222f875aebe7
George Flores <georgeflores886@gmail.com>
2020-10-27 16:33:13 -0400

    updated api-route

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json)
+app.use(express.json())
+
+require('./controllers/api-routes.js')(app)
 
 
 const XIVAPI = require('xivapi-js')
 const xiv = new XIVAPI()
 
-const searchItem = async () => {
-    let res = await xiv.search('Stuffed Khloe')
-    console.log(res.Results[0])
-    return res.Results
-}
-searchItem()
+// const searchItem = async () => {
+//     let res = await xiv.search('Stuffed Khloe')
+//     console.log(res.Results[0])
+//     return res.Results
+// }
+// searchItem()
 
 app.listen(PORT, function () {
     console.log(`Server listening on port ${PORT}`)
